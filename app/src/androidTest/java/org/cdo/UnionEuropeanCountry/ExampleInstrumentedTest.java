8fcf6a0f58799d70afc0441d37e79f668faebb5f
gabrielezagarella <gabry-zaga20@hotmail.it>
2019-12-17 16:55:55 +0100

    add logoShape

--- app/src/androidTest/java/org/cdo/UnionEuropeanCountry/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/cdo/UnionEuropeanCountry/ExampleInstrumentedTest.java
@@ -24,6 +24,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("org.cdo.criminalintent", appContext.getPackageName());
+        assertEquals("org.cdo.UnionEuropeanCountry", appContext.getPackageName());
     }
 }
